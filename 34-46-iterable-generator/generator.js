5d190220e5171745cec0785e665e0b7abb9cd19e
agnes0304 <jiwoochoi0304@gmail.com>
2023-12-23 15:06:57 +0900

    added question

--- 34-46-iterable-generator/generator.js
+++ 34-46-iterable-generator/generator.js
@@ -1,4 +1,9 @@
 // leetcode finbonacci generator
+
+/* Write a generator function that returns a generator object which yields the fibonacci sequence.
+The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13. *
+
 /**
  * @return {Generator<number>}
  */
